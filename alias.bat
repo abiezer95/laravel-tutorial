d1f7dd80e9f90eefa16e9af6d4317d5aa399de56
abiezer <areyes@farmahorro.com.do>
2019-02-08 15:25:13 -0400

    factories and more updates

--- alias.bat
+++ alias.bat
@@ -1,6 +1,7 @@
 doskey l=dir $*
+::php artisan commands
 doskey art=php artisan $*
-doskey art-up=php artisan serve
+doskey art-up=php artisan serve $*
 doskey art:seed=php artisan db:seed $*
 doskey artm:seed=php artisan make:seeder $*
 doskey artm:mt=php artisan make:test $*
@@ -9,13 +10,34 @@ doskey art:mi=php artisan migrate $*
 doskey art:fr=php artisan migrate:fresh $*
 doskey art:ref=php artisan migrate:refresh $*
 doskey art:re=php artisan migrate:reset $*
-doskey cm=commit -m $*
+doskey artm:auth=art:cmd=php artisan make:auth $*
+doskey artm:cmd=php artisan make:command $*
+doskey artm:controller=php artisan make:controller $*
+doskey artm:event=php artisan make:event $*
+doskey artm:job=php artisan make:job $*
+doskey artm:listener=php artisan make:listener $*
+doskey artm:mail=php artisan make:mail $*
+doskey artm:middleware=php artisan make:middleware $*
+doskey artm:migration=php artisan make:migration $*
+doskey artm:notification=php artisan make:notification $*
+doskey artm:policy=php artisan make:policy $*
+doskey artm:provider=php artisan make:provider $*
+doskey artm:request=php artisan make:request $*
+doskey art:tinker=php artisan tinker $*
+::git commands
+doskey cm=git commit -m $*
 doskey st=git status $*
 doskey push=git push origin $*
 doskey push=git push origin $*
 doskey pull=git pull $*
+doskey br=git branch $*
+doskey ct=git checkout $*
+doskey ctb=git checkout -b $*
 doskey aa=git add -A $*
 doskey mg=git merge $*
 doskey tag=git tag -a $*
 doskey tagl=git tag -l $*
+doskey log=git log --oneline $*
+doskey logd=git log --oneline --decorate $*
+doskey df=git diff $*
 cls
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
+use \App\Profession; //archivo profession del model que cree
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,29 +19,37 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
-       // dd('ProfessionSeeder');
-      
-       $this->clean();
-       
-       //DB::insert("INSERT into professions(title) values(:title)", ['title'=>'hola mundo']); //
-      
-       DB::table('professions')->insert([
-         'title' => 'test1',
+    // dd('ProfessionSeeder');
+    
+    $this->clean();
+    
+    //DB::insert("INSERT into professions(title) values(:title)", ['title'=>'hola mundo']); //
 
-       ]);
+    Profession::Create([
+        'title' => 'backend-developer'
+        ]);
+//dos formas
+    \App\Profession::Create([
+            'title' => 'backend-developer'
+        ]); // esto es mas dinamico, usamos el model de laravel para agregar contenido dinamicamente
+    
+    //DB::table('professions')->insert([;
+        //'title' => 'test1'
+    //]);
 
-       //$val = DB::select("SELECT title from professions where title = ?", ['test1']); //se usa un marcador ? para prevenir ataques de inyeccion
-       //$val = DB::table('professions')->select('id')->take(1)->get(); //the same as select 
-       //$val = DB::table('professions')->where('title', 'test1')->value('id'); //the same as up 
-       $val = DB::table('professions')->whereTitle('test1')->value('id'); //the same as up pero con whereTitle donde title es el campo  
-       dd($val);
-       //dd($val->first()->id);
-       DB::table('users')->insert([
-            'name' => 'abiezer',
-            'email' => 'a@a.com',
-            'password' => bcrypt('secret'),
-            'profession_id' => $val
-       ]);
+    //$val = DB::select("SELECT title from professions where title = ?", ['test1']); //se usa un marcador ? para prevenir ataques de inyeccion
+    //$val = DB::table('professions')->select('id')->take(1)->get(); //the same as select 
+    //$val = DB::table('professions')->where('title', 'test1')->value('id'); //the same as up 
+    $val = DB::table('professions')->whereTitle('test1')->value('id'); //the same as up pero con whereTitle donde title es el campo  
+    dd($val);
+    //dd($val->first()->id);
+    DB::table('users')->insert([
+        'name' => 'abiezer',
+        'email' => 'a@a.com',
+        'password' => bcrypt('secret'),
+        'profession_id' => $val
+    ]);
+
         //$this->call(ProfessionSeeder::class);
     }
 }
