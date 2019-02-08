3f26b3b4bdc01aebb622f851a5fbcc299953504b
abiezer <areyes@farmahorro.com.do>
2019-02-08 10:46:32 -0400

    New doskey file added

---
+++ alias.bat
@@ -0,0 +1,21 @@
+doskey l=dir $*
+doskey art=php artisan $*
+doskey art-up=php artisan serve
+doskey art:seed=php artisan db:seed $*
+doskey artm:seed=php artisan make:seeder $*
+doskey artm:mt=php artisan make:test $*
+doskey artm:mo=php artisan make:model $*
+doskey art:mi=php artisan migrate $*
+doskey art:fr=php artisan migrate:fresh $*
+doskey art:ref=php artisan migrate:refresh $*
+doskey art:re=php artisan migrate:reset $*
+doskey cm=commit -m $*
+doskey st=git status $*
+doskey push=git push origin $*
+doskey push=git push origin $*
+doskey pull=git pull $*
+doskey aa=git add -A $*
+doskey mg=git merge $*
+doskey tag=git tag -a $*
+doskey tagl=git tag -l $*
+cls
